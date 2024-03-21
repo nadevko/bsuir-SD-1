a408662494af1daa5ccae7e692a61e7303de2d7b
Nadeŭka <nadevko@riseup.net>
2024-03-21 22:48:47 +0300

    complete lab 1

---
+++ 1/1.cs
@@ -0,0 +1,25 @@
+﻿namespace sd1._1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int money = 0;
+        Console.Write("Товар стоит\n\tРублей: ");
+        money -= 100 * Convert.ToInt32(Console.ReadLine());
+        Console.Write("\tКопеек: ");
+        money -= Convert.ToInt32(Console.ReadLine());
+        Console.Write("Было оплачено\n\tРублей: ");
+        money += 100 * Convert.ToInt32(Console.ReadLine());
+        Console.Write("\tКопеек: ");
+        money += Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(
+            $"{(
+                money < 0 ? "Задолженность" : "Остаток"
+            )}:\n\tРублей: {
+            Math.Abs(money / 100)
+        }\n\tКопеек: {
+            Math.Abs(money % 100)
+        }");
+    }
+}
