c8e43b8b87b749e831663cc3673798894ed3747b
Nadeŭka <nadevko@riseup.net>
2024-03-30 23:05:46 +0300

    complete lab 2

---
+++ 2/1.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace sd1._2;
+
+internal class Program
+{
+    private static bool Validate(string pin) =>
+        new Regex(@"^\d{4}(\d{2})?$").IsMatch(pin);
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        Console.WriteLine(
+            Validate(Console.ReadLine() ?? "")
+                ? "Это пин-код"
+                : "Что-то не то");
+    }
+}
