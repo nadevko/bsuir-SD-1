e2dc62e3ac8a74e9f411f7575136381f7a7fcd63
Nadeŭka <nadevko@riseup.net>
2024-09-29 20:46:40 +0300

    lab13: Complete

---
+++ 13/Program1.cs
@@ -0,0 +1,30 @@
+﻿namespace sd1._13;
+
+internal partial class Program1
+{
+    static void Main(string[] args)
+    {
+        var path = Path.GetFullPath(args[0]);
+
+        try
+        {
+            StreamWriter writer = new(path);
+            string? input;
+
+            Console.WriteLine("Enter strings to write to the file");
+            Console.WriteLine("To finish input, leave the string empty and press Enter\n");
+
+            while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+                writer.WriteLine(input);
+            writer.WriteLine();
+            writer.Close();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Error: {exception.Message}");
+            Environment.Exit(1);
+        }
+
+        Console.WriteLine($"File written successfully to {path}");
+    }
+}
---
+++ 13/Program2.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace sd1._13;
+
+internal partial class Program2
+{
+    static void Main(string[] args)
+    {
+        var path = Path.GetFullPath(args[0]);
+
+        try
+        {
+            string? input;
+            var sb = new StringBuilder();
+
+            Console.WriteLine("Enter strings to write to the file");
+            Console.WriteLine("To finish input, leave the string empty and press Enter\n");
+
+            while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+                sb.AppendLine(input);
+
+            var data = JsonDocument.Parse(sb.ToString());
+            File.WriteAllText(
+                path,
+                JsonSerializer.Serialize(data, new JsonSerializerOptions() { WriteIndented = true })
+            );
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Error: {exception.Message}");
+            Environment.Exit(1);
+        }
+
+        Console.WriteLine($"File written successfully to {path}");
+    }
+}
